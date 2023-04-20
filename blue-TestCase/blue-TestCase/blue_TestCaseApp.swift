0db479e2b40fb67679bf50c5e34e8334eaa51b2c
Amin <aminallah.bakhshaei@gmail.com>
2023-04-20 06:22:13 +0330

    inital commit - Hello world!

---
+++ blue-TestCase/blue-TestCase/blue_TestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  blue_TestCaseApp.swift
+//  blue-TestCase
+//
+//  Created by Amin on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct blue_TestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
